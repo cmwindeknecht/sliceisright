9d1f82bac16cace3f0172e2dcdb3693f68a5625d
Robert Terwilliger <cmwindeknecht@gmail.com>
2025-10-29 14:53:00 -0400

    tested duration / added cors for railway

--- src/main/java/com/SliceIsRight/Constants.java
+++ src/main/java/com/SliceIsRight/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public static final String ADMIN_PRIVILEGES = "Admin";
     public static final long ADMIN_TOKEN_DURATION = 720L; // 12 hours
     public static final String USER_PRIVILEGES  = "User";
-    public static final long USER_TOKEN_DURATION = 30L; // 1/2 hour
+    public static final long USER_TOKEN_DURATION = 1L; // 1 hour
     
     public static final String ENV_ADMIN_SETUP_TOKEN = "ADMIN_SETUP_TOKEN";
     public static final String ENV_JWT_SIGNING_KEY = "JWT_SIGNING_KEY";
